090d5037594d8197f432ddd3e0f17d11d7ca770d
Sylvain Viollon <thefunny@gmail.com>
2010-05-13 14:02:14 +0200

    Remove more table stuff

--- src/zeam/form/layout/__init__.py
+++ src/zeam/form/layout/__init__.py
@@ -1,6 +1,5 @@
 
 from zeam.form.base import *
 from zeam.form.composed import *
-from zeam.form.table import *
 
-from zeam.form.layout.form import Form, ComposedForm, TableForm
+from zeam.form.layout.form import Form, ComposedForm
